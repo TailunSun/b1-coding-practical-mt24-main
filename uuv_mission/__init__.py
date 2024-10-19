c8c3908156a1ec2840d16ce90974f2946020e1c9
TailunSun <tailun.sun@bnc.ox.ac.uk>
2024-10-19 15:40:58 +0100

    Delete unused code in init

--- uuv_mission/__init__.py
+++ uuv_mission/__init__.py
@@ -1,9 +1,1 @@
-from dynamic import Mission
 
-
-mission = Mission.from_csv('data/mission.csv')
-
-# Print the first few elements of each attribute to verify
-print(mission.reference[:5])  # Check the first 5 reference points
-print(mission.cave_height[:5])  # Check the first 5 cave height values
-print(mission.cave_depth[:5])  # Check the first 5 cave depth values
